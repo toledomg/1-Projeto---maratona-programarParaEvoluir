d9e470645a4c2b849cf66f40c1465bf1ab094c28
toledomg <toledomg@yahoo.com.br>
2022-08-28 10:52:29 -0300

    1° Projeto

---
+++ src/script/database.js
@@ -0,0 +1,42 @@
+const personagens = [
+  {
+    id: "0",
+    name: "boo",
+    img: "./src/img/Boo.png",
+  },
+  {
+    id: "1",
+    name: "goomba",
+    img: "./src/img/Goomba.png",
+  },
+  {
+    id: "2",
+    name: "shell",
+    img: "./src/img/Shell.png",
+  },
+  {
+    id: "3",
+    name: "piranha",
+    img: "./src/img/Piranha.png",
+  },
+  {
+    id: "4",
+    name: "toad",
+    img: "./src/img/Toad.png",
+  },
+  {
+    id: "5",
+    name: "yoshi",
+    img: "./src/img/Yoshi.png",
+  },
+  {
+    id: "6",
+    name: "Luigi",
+    img: "./src/img/Luigi.png",
+  },
+  {
+    id: "7",
+    name: "bowser",
+    img: "./src/img/Bowser.png",
+  },
+];
---
+++ src/script/main.js
@@ -0,0 +1,7 @@
+const main = document.getElementById("main");
+const buttonJogar = document.getElementById("Bjogar");
+// console.log(buttonJogar);
+
+buttonJogar.addEventListener("click", () => {
+  window.location.assign("../../jogo.html");
+});
