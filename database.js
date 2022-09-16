99ee1d9cb2dca401568d853d69d9a2a91bdfc59a
toledomg <toledomg@yahoo.com.br>
2022-09-16 11:18:07 -0300

    teste

---
+++ database.js
@@ -0,0 +1,22 @@
+const personagens = [
+  {
+    id: "0",
+    name: "bb8",
+    img: "./src/img/bb8.jpg",
+  },
+  {
+    id: "1",
+    name: "r2d2",
+    img: "./src/img/r2d2.jpg",
+  },
+  {
+    id: "2",
+    name: "sabreVerde",
+    img: "./src/img/sabreVerde.jpg",
+  },
+  {
+    id: "3",
+    name: "chewb",
+    img: "./src/img/chewb.jpg",
+  },
+];
